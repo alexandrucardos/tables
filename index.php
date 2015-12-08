3a8c91fbe4151f9edb47c234fb8fca67a7752c3a
alexandru.cardos <alec.cardos@gmail.com>
2015-12-08 18:46:21 +0200

    commit cu select pe tabela task

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <head></head>
 <body>
-<td>
-    <tr> te </tr>
-    <tr> tt </tr>
-
-</td>
+<h1>
+    This is a list of things to buy
+    <br>
+</h1>
+<p>
+    Items from the list to buy
+</p>
 </body>
 <?php
 include '.ignore/login.php';
+$test = "select * from taskuri";
+$result =$con->query($test);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["idTask"]. " - Name: " . $row["numeTask"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 ?>
 
