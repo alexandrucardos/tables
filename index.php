8a5f167910feff73e46e25e948651d84007c8028
alexandru.cardos <alec.cardos@gmail.com>
2015-12-09 19:19:42 +0200

    'culori'

--- index.php
+++ index.php
@@ -1,28 +1,55 @@
 <head></head>
 <body>
+<style>
+    table,tr,td{
+        width: 100%;
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    .thick_line_column{
+        width: 10%;
+        border: 2px solid ;
+    }
+    .red_cell{
+        border: 2px solid red;
+    }
+    .green_cell{
+        border: 2px solid green;
+    }
+</style>
+
 <h1>
     This is a list of things to buy
     <br>
 </h1>
-<p>
-    Items from the list to buy
-</p>
+<h2>
+    Presents for important persons
+</h2>
+
 </body>
 <?php
 include '.ignore/login.php';
 $test = "SELECT * FROM cadouri";
 $result =$con->query($test);
-
+echo"<table>";
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<td>";
-        echo "<tr>Nume:---" . $row["nume"]."</tr>".
-            "<tr>--------" . $row["cadou"]."</tr>"."<br>";
-        echo "</td>";
+        echo '<tr>';
+        echo "<td class='thick_line_column'>" . $row["nume"]."</td>".
+            "<td>..." . $row["cadou"]."</td>".
+            "<td";
+            if($row["status"]==1){
+                echo' class = "green_cell"';
+            }else {echo' class = "red_cell"';}
+            echo">".$row["status"]."</td>".
+            "</tr>";
     }
+    echo"</table>";
+
 } else {
     echo "0 results";
-}
+};
+
 ?>
 
