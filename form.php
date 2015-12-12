4dbfecae55a2285877209b9c38880bd77813074d
alexandru.cardos <alec.cardos@gmail.com>
2015-12-12 08:14:23 +0200

    'sdf'

--- form.php
+++ form.php
@@ -1,56 +1,8 @@
-<head></head>
+<html>
 <body>
-<style>
-    table,tr,td{
-        font-size: 20px;
-        width: 100%;
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-    .thick_line_column{
-        width: 30%;
-        border: 2px solid ;
-    }
-    .red_cell{
-        border: 2px solid red;
-    }
-    .green_cell{
-        border: 2px solid green;
-    }
-</style>
 
-<h1>
-    This is a list of things to buy
-    <br>
-</h1>
-<h2>
-    Presents for important persons
-</h2>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
-<?php
-include '.ignore/login.php';
-$test = "SELECT * FROM cadouri";
-$result =$con->query($test);
-echo"<table>";
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo '<tr>';
-        echo "<td class='thick_line_column'>" . $row["nume"]."</td>".
-            "<td>..." . $row["cadou"]."</td>".
-            "<td";
-        if($row["status"]==1){
-            echo' class = "green_cell"';
-        }else {echo' class = "red_cell"';}
-        echo">".$row["status"]."</td>".
-            "</tr>";
-    }
-    echo"</table>";
-
-} else {
-    echo "0 results";
-};
-
-?>
-
+</html>
--- index.php
+++ index.php
@@ -1,56 +1,32 @@
-<head></head>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
 <body>
 <style>
-    table,tr,td{
-        font-size: 20px;
-        width: 100%;
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-    .thick_line_column{
-        width: 30%;
-        border: 2px solid ;
-    }
-    .red_cell{
-        border: 2px solid red;
-    }
-    .green_cell{
-        border: 2px solid green;
-    }
+
 </style>
 
 <h1>
-    This is a list of things to buy
+    Formular cadouri
     <br>
 </h1>
-<h2>
-    Presents for important persons
-</h2>
+
+<form action ="form.php"  method="post">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Nume persoana</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Cadou</label>
+        <input type="text" class="form-control" id="cadou" name="cadou" placeholder="Cadou">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputEmail1">status</label>
+        <input type="number" class="form-control" id="cadou" name="cadou" placeholder="status">
+    </div>
+    <button class="btn btn-default"><input type="submit">Submit </button>
+</form>
+
 
 </body>
-<?php
-include '.ignore/login.php';
-$test = "SELECT * FROM cadouri";
-$result =$con->query($test);
-echo"<table>";
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo '<tr>';
-        echo "<td class='thick_line_column'>" . $row["nume"]."</td>".
-            "<td>..." . $row["cadou"]."</td>".
-            "<td";
-            if($row["status"]==1){
-                echo' class = "green_cell"';
-            }else {echo' class = "red_cell"';}
-            echo">".$row["status"]."</td>".
-            "</tr>";
-    }
-    echo"</table>";
-
-} else {
-    echo "0 results";
-};
-
-?>
 
