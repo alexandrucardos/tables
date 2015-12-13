750afa42d805668a26789c0f6eaca00f99c5147a
alec.server <alec.cardos@gmail.com>
2015-12-13 08:13:27 +0200

    adaugat si proiecte saptamanale

--- index.php
+++ index.php
@@ -3,11 +3,15 @@
 </head>
 <body>
 <style>
-
+    .div_main{
+        max-width: 600px;
+        margin-left: auto;
+        margin-right: auto;
+    }
 </style>
-
+<div class="div_main">
 <h1>
-    This is a list of things to buy
+    This is a list of things to buy and do
     <br>
 </h1>
 <h2>
@@ -19,7 +23,7 @@
 include '.ignore/login.php';
 $test = "SELECT * FROM cadouri";
 $result =$con->query($test);
-echo'<table class = "table table-striped table-bordered table-responsive table-hover">';
+echo'<table class = "table table-striped table-bordered table-responsive table-hover col-md-3 ">';
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
@@ -37,6 +41,28 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 };
+?>
+<?
+$proiecte_saptamanale = "SELECT  pnotlog.idProiect, pnotlog.numeProiect,  COALESCE(plog.Timp_minute,0) AS 'Timp logat' , pnotlog.Timp_necesar_minute - COALESCE(plog.Timp_minute, 0) AS 'Timp ramas' , (case when pnotlog.Timp_necesar_minute - COALESCE(plog.Timp_minute, 0)<=0 then 'Done' else NULL end) as 'Finalizate'
+FROM proiecte pnotlog
+LEFT JOIN
+		(
+			SELECT p.idProiect, l.`Data`,SUM(l.Timp_minute) AS Timp_minute
+			FROM proiecte p
+			LEFT JOIN log_timp l ON l.Id_proiect =p.idProiect
+			WHERE p.StatusCompletare IN (2)
+			AND (week(l.`Data`-interval 1 day)= week(CURRENT_DATE()-interval 1 day))
+			GROUP BY p.idProiect
+		) plog ON plog.idProiect = pnotlog.idProiect
+WHERE pnotlog.StatusCompletare IN (2);";
+$proiecte_qry = $con->query($proiecte_saptamanale);
+echo'<table class = "table table-striped table-bordered table-responsive table-hover col-md-3 ">';
+while($proiecte_fetch=$proiecte_qry->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>".$proiecte_fetch['idProiect']."</td>";
+    echo "<td>".$proiecte_fetch['numeProiect']."</td>";
+    echo "<td><strong>".$proiecte_fetch['Timp ramas']."</strong></td>";
+};
 
 ?>
-
+</div>
